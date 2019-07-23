665d6e6cc8f9ce86026c901bed538fa134dca272
Otim Fredrick <fredrickot@gmail.com>
2019-07-23 19:02:50 +0300

    initial project setup

---
+++ Dev/javascript/Vue/quotes/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="container">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      quotes: ['First quote to display'],
+      maxQuote: 10
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
