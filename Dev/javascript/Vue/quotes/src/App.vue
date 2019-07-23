3f016da388f28fc54be3c281113b272b8d900f83
Otim Fredrick <fredrickot@gmail.com>
2019-07-23 20:26:36 +0300

    can limit added quotes

--- Dev/javascript/Vue/quotes/src/App.vue
+++ Dev/javascript/Vue/quotes/src/App.vue
@@ -1,7 +1,18 @@
 <template>
   <div id="container">
-    <app-new-quote @quoteAdded="newQuote($event)"></app-new-quote>
-    <app-quote-grid :quotes="quotes"></app-quote-grid>
+    <div class="row">
+      <app-header :quoteCount="quotes.length" :maxQuote="maxQuote"></app-header>
+      <app-new-quote @quoteAdded="newQuote($event)"></app-new-quote>
+      <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote($event)"></app-quote-grid>
+    </div>
+    
+    <div class="row">
+      <div class="col sm-12 textcenter">
+        <div class="alert alert-info">
+          Info Click on a quote to delete it!
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,6 +20,7 @@
 
   import QuoteGrid from './components/QuoteGrid.vue';
   import NewQuote from './components/NewQuote.vue';
+  import Header from './components/Header.vue';
 
   export default {
     data: function() {
@@ -21,12 +33,19 @@
     },
     methods: {
       newQuote(quote) {
+        if(this.quotes.length >= this.maxQuote) {
+          return alert('Please delete Quotes first');
+        }
         this.quotes.push(quote);
+      },
+      deleteQuote(index) {
+        this.quotes.splice(index, 1)
       }
     },
     components: {
       appQuoteGrid: QuoteGrid,
-      appNewQuote: NewQuote
+      appNewQuote: NewQuote,
+      appHeader: Header
     }
   }
 </script>
